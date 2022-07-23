ec087fbc5fef478f7531a29a9f6b3ae8a8875a5c
mglazik <fotografia.m@gmail.com>
2022-07-23 02:33:05 +0200

    rekurencja

--- fi.py
+++ fi.py
@@ -4,6 +4,16 @@
 import fire
 
 
+def fibonacci(a,b,i):
+    c = a+b
+    print(c)
+    i=i-1
+    if i == 0:
+        pass
+    else:
+        return fibonacci(b,c,i)
+    
+         
 def wynik(amount):
     x = 0
     a = 0
@@ -21,13 +31,10 @@ def wynik(amount):
         
         x += 1
 
-def dodaj(a, b):
-    
+def dodaj(a, b):   
     c = a + b
     return c
    
-
-
 
 class fibo(object):
     def fi(self, amount):
@@ -41,7 +48,21 @@ class fibo(object):
  #       if amount >= 3:
 
         return wynik(amount)
-        
+    
+    def reku(self, ilosc):
+        if ilosc < 0:
+            print("wybierz liczbę większą od zera")
+        if ilosc == 0:
+            print("0")
+            pass
+        if ilosc == 1:
+            print("0")
+            print("1")
+            pass
+        if ilosc >= 2:
+            print("0")
+            print("1")
+            fibonacci (0,1,ilosc-1)
       
 #####
 if __name__ == '__main__':
